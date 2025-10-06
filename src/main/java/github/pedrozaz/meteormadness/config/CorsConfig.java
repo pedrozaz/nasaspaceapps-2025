2584962185ea3f6f901be6f81783c291cfecfd90
Guilherme <guilhermebarb0sa@proton.me>
2025-10-05 21:49:43 -0300

    Cors config

--- src/main/java/github/pedrozaz/meteormadness/config/CorsConfig.java
+++ src/main/java/github/pedrozaz/meteormadness/config/CorsConfig.java
@@ -1,16 +1,21 @@
 package github.pedrozaz.meteormadness.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:5173", "http://127.0.0.1:5173")
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedOriginPatterns(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
